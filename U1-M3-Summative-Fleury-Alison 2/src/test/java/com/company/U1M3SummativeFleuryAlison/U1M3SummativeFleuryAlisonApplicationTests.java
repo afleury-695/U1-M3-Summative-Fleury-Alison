0fe49069699fa20e390174ac93641eccb514a3cd
Cecilia Ames <afleury@argyletech.net>
2019-07-18 14:15:32 -0500

    Add spring project into folder on week 4 day 2

---
+++ U1-M3-Summative-Fleury-Alison 2/src/test/java/com/company/U1M3SummativeFleuryAlison/U1M3SummativeFleuryAlisonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.U1M3SummativeFleuryAlison;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class U1M3SummativeFleuryAlisonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
